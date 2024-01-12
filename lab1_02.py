4b7786460acbaafcba4450bc461b1451092cfb9b
Ananya Sharma <as1109work@gmail.com>
2024-01-12 23:56:10 +0530

    Create lab1_02.py
    11/01/24

---
+++ lab1_02.py
@@ -0,0 +1,13 @@
+#wap to assign or initialize two integer values then perform the basic arithmetic operation like addition subtraction multiplication and division
+num1= input('first number:')
+num2=input('second number:')
+
+sum= int(num1)+int(num2)
+mul= int(num1)*int(num2)
+div= int(num1)/int(num2)
+sub= int(num1)-int(num2)
+
+print("SUM:{0}".format(sum))
+print("MULTIPLICATION:{0}".format(mul))
+print("DIVISION:{0}".format(div))
+print("SUB:{0}".format(sub))
