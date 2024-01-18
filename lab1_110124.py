872e97b49daab51586fbc7eb4b027cfbfbc9d05c
Ananya Sharma <as1109work@gmail.com>
2024-01-18 20:46:09 +0530

    Update and rename lab1_01.py to lab1_110124.py

---
+++ lab1_110124.py
@@ -0,0 +1,26 @@
+#wap to display your roll number ,name and section in different lines
+print("Enter your name:")
+x = input()
+print("NAME:"+x)
+print("Enter your roll number:")
+y = input()
+print("ROLL:"+y)
+print("Enter your section:")
+z = input()
+print("SECTION:"+z)
+
+
+
+#wap to assign or initialize two integer values then perform the basic arithmetic operation like addition subtraction multiplication and division
+num1= input('first number:')
+num2=input('second number:')
+
+sum= int(num1)+int(num2)
+mul= int(num1)*int(num2)
+div= int(num1)/int(num2)
+sub= int(num1)-int(num2)
+
+print("SUM:{0}".format(sum))
+print("MULTIPLICATION:{0}".format(mul))
+print("DIVISION:{0}".format(div))
+print("SUB:{0}".format(sub))
