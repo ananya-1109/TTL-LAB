102147f5211815ad56b84c9ff04daa61244fc2cc
Ananya Sharma <as1109work@gmail.com>
2024-02-01 10:57:41 +0530

    Create q6.py

---
+++ lab_010224/q6.py
@@ -0,0 +1,6 @@
+#6.write a python program to find the number of items available in the list. also find the largest number from the list.
+
+list=[1,2,3,4,5,10]
+print(len(list))
+largest=max(list)
+print(largest)
