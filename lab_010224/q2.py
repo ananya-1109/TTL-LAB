7d56f4df09341d87422b774d4a6144e14b81ef84
Ananya Sharma <as1109work@gmail.com>
2024-02-01 10:56:06 +0530

    Create q2.py

---
+++ lab_010224/q2.py
@@ -0,0 +1,5 @@
+#2.write a python program to display a set of characters of a string according to the given range. also find out a substring within a string.
+
+str="abcdefghijklmnopqrstuvwxyz"
+print(str[:6])
+print("mno" in str)
