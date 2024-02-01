10cb0acc36d8225413d0640d570d1527805b18dc
Ananya Sharma <as1109work@gmail.com>
2024-02-01 10:55:38 +0530

    Create q1.py

---
+++ lab_010224/q1.py
@@ -0,0 +1,5 @@
+#1.write a python program to display a character of a string according to the give index. also display a character according to their given reverse indexing(negative indexing)
+
+str="abcdefgh"
+print(str[2])
+print(str[-4])
