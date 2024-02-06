741a5ff32279e44c7d12e5eb7395f2fea5dc4ac9
Ananya Sharma <as1109work@gmail.com>
2024-02-06 21:53:36 +0530

    Delete lab_010224/q10.py

--- lab_010224/q10.py
+++
@@ -1,9 +0,0 @@
-#10.write a python program to convert a tuple to a string then create a list where all the items of the list are takem from keyboard dynamically then convert that list to a tuple.
-t=("Ana","nya"," Sharma")
-l=[]
-for i in t:
-    l.append(i)
-print(l)
-s=" "
-s1=s.join(l)
-print(s1)
