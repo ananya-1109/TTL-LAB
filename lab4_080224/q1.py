e07bd356cc64facada5ff4660961e6fefdc66045
Ananya Sharma <as1109work@gmail.com>
2024-02-09 22:39:04 +0530

    Create q1.py

---
+++ lab4_080224/q1.py
@@ -0,0 +1,10 @@
+#1.create a set to store a set of elements and perform the set operations such as intersection, union and difference.
+
+set1={1,2,3,4,5,6,7,8,9,10}
+set2={11,4,21,5,8}
+rel1=set1|set2
+print(rel1)
+rel2=set1&set2
+print(rel2)
+rel3=set1-set2
+print(rel3)
